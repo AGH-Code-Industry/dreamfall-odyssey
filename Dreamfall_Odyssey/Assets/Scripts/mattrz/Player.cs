ab4425e1af68f05f0bf77301314899de22936243
Zyx750 <maciej.trzaskacz@gmail.com>
2024-11-23 23:08:05 +0100

    Player sprite and basic movement

---
+++ Dreamfall_Odyssey/Assets/Scripts/mattrz/Player.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace mattrz
+{
+    public class Player : MonoBehaviour
+    {
+
+        [SerializeField] private float speed = 8f;
+        [SerializeField] private Animator animator;
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private float groundCheckDistance = 1.2f;
+
+        private static readonly int IsRunning = Animator.StringToHash("IsRunning");
+        private static readonly int IsInAir = Animator.StringToHash("IsInAir");
+    
+        private float _horizontalVelocity;
+        private Rigidbody2D _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+        
+        private void Update()
+        {
+            _rigidbody.linearVelocity = new Vector2(_horizontalVelocity, _rigidbody.linearVelocity.y);
+            
+            if(IsGrounded())
+            {
+                animator.SetBool(IsInAir, false);
+            }
+            else
+            {
+                animator.SetBool(IsInAir, true);
+            }
+        }
+
+        public void OnMove(InputAction.CallbackContext ctx)
+        {
+            _horizontalVelocity = ctx.ReadValue<float>() * speed;
+            if (animator)
+            {
+                if (_horizontalVelocity > 0)
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                } else if (_horizontalVelocity < 0)
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
+                
+                if (Mathf.Abs(_horizontalVelocity) > 0.01f)
+                {
+                    animator.SetBool(IsRunning, true);
+                }
+                else
+                {
+                    animator.SetBool(IsRunning, false);
+                }
+            }
+        }
+        
+        public void OnJump(InputAction.CallbackContext ctx)
+        {
+            if (ctx.performed && IsGrounded())
+            {
+                _rigidbody.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+            }
+        }
+        
+        private bool IsGrounded()
+        {
+            return Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
+        }
+    }
+}
