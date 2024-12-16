30b8462d49a484f0e58283928bd7bc2331baf393
Terry02pl <blazejhej@gmail.com>
2024-12-16 22:39:04 +0100

    epic character and enemies

--- Dreamfall_Odyssey/Assets/enemy_ctrl.cs
+++ Dreamfall_Odyssey/Assets/enemy_ctrl.cs
@@ -4,14 +4,14 @@ using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.U2D.IK;
-using UnityEngine.UI;
 
-public class player_ctrler : MonoBehaviour
+public class enemy_ctrl : MonoBehaviour
 {
 
     private Collider2D cc;
     private Rigidbody2D rb;
     [SerializeField] private float speed = 6.0f;
+    [SerializeField] private float wallCheck = 2.0f;
     [SerializeField] private float time_Apex = 1.0f;
     
     [SerializeField] private float time_Coyote = 1.0f;
@@ -22,48 +22,49 @@ public class player_ctrler : MonoBehaviour
     private bool onGround;
     private float airtime;
     [SerializeField] private Transform feet;
+    [SerializeField] private Transform tgt_1;
+    [SerializeField] private Transform tgt_2;
+    private Transform current_track;
     [SerializeField] private LayerMask ground;
-    
-    [SerializeField] private LayerMask hazard;
-    [SerializeField] private GameObject dead;
     private bool jumpBuffer = false;
-    private bool lockChar = false;
     public float jumpBufferTime = 0.1f;
     private float jumpBufferTimer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cc = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
         airtime = time_Apex;
+        current_track = tgt_1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Physics2D.OverlapBox(transform.position, Vector2.one,0f,hazard))
+        float orientation = Mathf.Abs(current_track.position.x - transform.position.x);
+        if(orientation < 1f)
         {
-            rb.freezeRotation = false;
-            rb.linearVelocityY = 20f;
-            rb.linearVelocityX = Random.Range(-20f,20f);
-            rb.angularVelocity = Random.Range(-200f,200f);
-            lockChar = true;
-            dead.SetActive(true);
+            if(current_track == tgt_1)
+            {
+                current_track = tgt_2;
+            }
+            else
+            {
+                current_track = tgt_1;
+            }
+
+            //Debug.Log("SWITCHING");
+            
         }
+        horizontal = Mathf.Sign(-transform.position.x + current_track.position.x);
+        
+        //Debug.Log(Mathf.Abs(current_track.position.x - transform.position.x));
 
-        horizontal = Input.GetAxisRaw("Horizontal"); 
-
-        if(jumpBufferTimer > 0)
-        {
-            jumpBufferTimer -= Time.deltaTime;
-        }
-        else
-            jumpBuffer = false;
-
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Physics2D.Raycast(transform.position, Vector2.left * orientation, wallCheck, ground) && IsGrounded())
         {
             jumpBufferTimer = jumpBufferTime;
-            jumpBuffer = true;
+            rb.linearVelocityY = jumpStrenght;
         }
 
         if(rb.linearVelocityY < 0.02f && !IsGrounded() && airtime >0)
@@ -72,22 +73,11 @@ public class player_ctrler : MonoBehaviour
             rb.linearVelocityY = 0.01f;
             jumpBufferTime = 0f;
         }
-
-        if(jumpBuffer && IsGrounded() && !lockChar)
-        {
-            rb.linearVelocityY = jumpStrenght;
-            airtime = time_Apex;
-        }
-        if(Input.GetKeyUp(KeyCode.Space) && rb.linearVelocityY > 0f)
-        {
-            rb.linearVelocityY = rb.linearVelocityY*0.5f;
-        }
     }
 
     private void FixedUpdate()
     {
-        if(!lockChar)
-            rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocityY);
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocityY);
     }
 
     private bool IsGrounded()
